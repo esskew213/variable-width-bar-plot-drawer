5ea5e7268eb79baf615bb9c78ef4b6df606e0153
Sarahqtw <Sarahqtw@hotmail.com>
2023-07-03 23:41:06 +0800

    colour chart by legend

--- draw.js
+++ draw.js
@@ -1,5 +1,7 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 const body = document.querySelector('body');
+const chartContainer = document.querySelector('#chart-container');
+console.log(chartContainer);
 const submitButton = document.body.querySelector('form input[type="submit"]');
 submitButton.addEventListener('click', handleSubmit);
 const fileInput = document.getElementById('dataFile');
@@ -25,6 +27,14 @@ fileInput.addEventListener('change', () => {
   }
 });
 
+function printErrorMessage(message) {
+  const errorMessage = document
+    .createElement('p')
+    .setAttribute('id', 'error-message');
+  const errorText = document.createTextNode(message);
+  body.append(errorMessage.appendChild(errorText));
+}
+
 function handleSubmit(e) {
   e.preventDefault();
   reader.readAsText(selectedFile);
@@ -33,23 +43,25 @@ function handleSubmit(e) {
 }
 
 function generateChart() {
+  d3.selectAll('svg').remove();
+
   const data = parseCSV();
   drawChart(data);
 }
 
 function parseCSV() {
   const data = d3.csvParse(reader.result, d3.autoType);
-  data.sort((a, b) => d3.ascending(a.height, b.height));
+  data.sort((a, b) => d3.ascending(a[data.columns[2]], b[data.columns[2]]));
   console.log(data);
   return data;
 }
 
 function handleError() {
-  console.log('error!!!');
+  printErrorMessage('Error uploading csv.');
 }
 
 function getBarCoordinates(data) {
-  const barWidths = data.map((item) => item.width);
+  const barWidths = data.map((item) => item[data.columns[1]]);
   const barCoordinates = [0];
   for (let i = 0; i < barWidths.length; i++) {
     barCoordinates.push(barCoordinates[i] + barWidths[i]);
@@ -57,12 +69,40 @@ function getBarCoordinates(data) {
   return barCoordinates;
 }
 
+function generateLegendColours(data) {
+  const COLOURLIST = [
+    '#F77F00',
+    '#D62828',
+    '#FCBF49',
+    '#EAE2B7',
+    '#003049',
+    '#588157',
+    '#38A3A5',
+    '#6D597A',
+    '#F07167',
+    '#DDA15E',
+  ];
+  try {
+    return COLOURLIST.slice(0, data.length);
+  } catch {
+    printErrorMessage(
+      'Too many categories in legend. Please use fewer than 10 categories.'
+    );
+  }
+}
+
 function drawChart(data) {
   const barCoordinates = getBarCoordinates(data);
+  const [
+    namesColumnName,
+    widthColumnName,
+    heightsColumnName,
+    legendColumnName,
+  ] = data.columns;
   const xMin = barCoordinates[0];
   const xMax = barCoordinates[barCoordinates.length - 1];
 
-  const [yMin, yMax] = d3.extent(data, (d) => d.height);
+  const [yMin, yMax] = d3.extent(data, (d) => d[heightsColumnName]);
 
   const xScale = d3
     .scaleLinear()
@@ -74,6 +114,11 @@ function drawChart(data) {
     .domain([yMin, yMax])
     .range([svgHeight - marginBottom, marginTop]);
 
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(data.map((d) => d[legendColumnName]))
+    .range(generateLegendColours(data))
+    .unknown('#8B8C89');
   // creating SVG
   const svg = d3
     .create('svg')
@@ -82,6 +127,34 @@ function drawChart(data) {
     .attr('viewBox', [0, 0, svgWidth, svgHeight])
     .attr('style', 'max-width: 100%; height: auto;');
 
+  // draw bars
+  svg
+    .append('g')
+    .attr('fill-opacity', '80%')
+    .attr('stroke', 'black')
+    .attr('stroke-width', '1px')
+    .selectAll()
+    .data(data)
+    .join('rect')
+    .attr('fill', (d) => colorScale(d[legendColumnName]))
+    .attr('y', (d) => {
+      if (d[heightsColumnName] < 0) {
+        return yScale(0);
+      } else {
+        return yScale(d[heightsColumnName]);
+      }
+    })
+    .attr('height', (d) => {
+      if (d[heightsColumnName] < 0) {
+        return yScale(d[heightsColumnName]) - yScale(0);
+      } else {
+        return yScale(0) - yScale(d[heightsColumnName]);
+      }
+    })
+    .attr('width', (d) => xScale(d[widthColumnName]) - marginLeft)
+    .attr('x', (d, i) => {
+      return xScale(barCoordinates[i]);
+    });
   //x axis
   svg
     .append('g')
@@ -95,7 +168,7 @@ function drawChart(data) {
         .attr('y', 30)
         .attr('fill', 'black')
         .attr('text-anchor', 'end')
-        .text(data.columns[1])
+        .text(widthColumnName)
     );
 
   //y axis
@@ -111,37 +184,8 @@ function drawChart(data) {
         .attr('y', 10)
         .attr('fill', 'currentColor')
         .attr('text-anchor', 'start')
-        .text(data.columns[2])
+        .text(heightsColumnName)
     );
 
-  // draw bars
-  svg
-    .append('g')
-    .attr('fill', 'steelblue')
-    .attr('fill-opacity', '20%')
-    .attr('stroke', 'black')
-    .attr('stroke-width', '1px')
-    .selectAll()
-    .data(data)
-    .join('rect')
-    .attr('y', (d) => {
-      if (d.height < 0) {
-        return yScale(0);
-      } else {
-        return yScale(d.height);
-      }
-    })
-    .attr('height', (d) => {
-      if (d.height < 0) {
-        return yScale(d.height) - yScale(0);
-      } else {
-        return yScale(0) - yScale(d.height);
-      }
-    })
-    .attr('width', (d) => xScale(d.width) - marginLeft)
-    .attr('x', (d, i) => {
-      return xScale(barCoordinates[i]);
-    });
-
-  body.append(svg.node());
+  chartContainer.append(svg.node());
 }
